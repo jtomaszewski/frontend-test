984c29222ef1b08f28041b982faa3f7afc28ca3a
Chris Applegate <chris@onlytwo.io>
2019-09-20 14:58:14 +0100

    Question

---
+++ src/CountryInfo.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function CountryInfo(props) {
+
+  return <React.Fragment>
+
+    { /* TODO: This is where the detail goes */ }
+
+  </React.Fragment>;
+
+}
