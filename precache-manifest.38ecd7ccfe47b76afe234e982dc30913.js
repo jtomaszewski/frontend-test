1a207441c839a49b521886f5a746f972feeb0208
Jacek Tomaszewski <jacek@jtom.me>
2019-09-24 00:02:29 +0200

    Updates

---
+++ precache-manifest.38ecd7ccfe47b76afe234e982dc30913.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7120f0b0bf3d57a0f5aa3389a877e121",
+    "url": "/frontend-test/index.html"
+  },
+  {
+    "revision": "a5c488e12c787b06fc42",
+    "url": "/frontend-test/static/css/main.ab962428.chunk.css"
+  },
+  {
+    "revision": "714218d3815f1ddcdf44",
+    "url": "/frontend-test/static/js/2.2b125c81.chunk.js"
+  },
+  {
+    "revision": "a5c488e12c787b06fc42",
+    "url": "/frontend-test/static/js/main.45c0b1e6.chunk.js"
+  },
+  {
+    "revision": "08efb999f954138e87f0",
+    "url": "/frontend-test/static/js/runtime-main.bb83c375.js"
+  }
+]);
